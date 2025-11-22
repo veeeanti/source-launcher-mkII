e8ea870467adddc42b55e5e5c7ebd13eeebac9c8
vee anti <113473346+veeeanti@users.noreply.github.com>
2025-11-21 20:05:20 -0600

    Create HL2 20th Anniv. branch

--- Half-Life 2/steam_legacy/episodic/hl2_launcher.h
+++
@@ -1,9 +0,0 @@
-#ifndef HL2_LAUNCHER_H
-#define HL2_LAUNCHER_H
-
-#include <string>
-
-// Function declarations
-bool fileExists(const std::string& filePath);
-
-#endif // HL2_LAUNCHER_H
--- Portal/portal_launcher.h
+++
@@ -1,9 +0,0 @@
-#ifndef PORTAL_LAUNCHER_H
-#define PORTAL_LAUNCHER_H
-
-#include <string>
-
-// Function declarations
-bool fileExists(const std::string& filePath);
-
-#endif // PORTAL_LAUNCHER_H

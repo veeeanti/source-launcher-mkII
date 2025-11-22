7dc91595f0aab1be385682d33a8fc5a3aa030bc0
vee anti <113473346+veeeanti@users.noreply.github.com>
2025-11-21 20:22:05 -0600

    black mesa blue shift branch

--- bms_launcher.cpp
+++ bms_launcher.cpp
@@ -28,16 +28,22 @@ int main() {
 
     // no bms.exe? PANIC!!! SOUND THE FUCKING ALARM!!!! AAHHHHHHHHHHHHH!!!!!!!!
     if (!fileExists(hl2ExePath)) {
-        MessageBoxA(NULL, "Error: bms.exe not found. Please place this file in the same directory as bms.exe. It seems you did not do that.", "HL2 Launcher Error", MB_OK | MB_ICONERROR);
+
+		// whoops, accidentally left a remnant of hl2 code - my bad. it *is* literally just my code for hl2 launcher, so it doesn't really matter to me.
+		// but it might confuse someone else seeing HL2 instead of BMS, soooo... (also, i didn't fix it yet for base Black Mesa so ignore it if you're reading this)
+        MessageBoxA(NULL, "Error: bms.exe not found. Please place this file in the same directory as bms.exe. It seems you did not do that.", "BMS Launcher Error", MB_OK | MB_ICONERROR);
         return 1;
     }
+
+    // This is a note for whenever it's run and no errors come up to let people know that Black Mesa Blue Shift takes a weirdly long time to fully launch and run, and to prevent people from running it more than once (though Source Engine already has a built in safeguard for that that only lets one instance of any Source Engine game run at any given time)
+    MessageBoxA(NULL, "Please give the game a few minutes to launch, do not run it again - it worked, it just takes a long time normally, I'm not sure why but it's just something that Blue Shift does. It's nothing caused by this nor is it anything on your end, so just please be patient. You can close this window or click 'OK' safely, it won't interrupt the game's launching.", "BMS Launcher - Heads up!", MB_OK | MB_ICONINFORMATION);
 
     // the actual running of bms.exe
     STARTUPINFOW si = { sizeof(si) };
     PROCESS_INFORMATION pi;
 
-	// Black Mesa launch command line arguments, -disabledvk and -forceuid3d9 are required or else the game will crash on launch. Thanks, crowbar collective.
-    std::wstring commandLine = L"bms.exe -game bms -disabledxvk -forceuid3d9";
+	// Black Mesa: Blue Shift launch command line arguments, -disabledvk and -forceuid3d9 are required or else the game will crash on launch. Thanks, crowbar collective.
+    std::wstring commandLine = L"bms.exe -game bshift -disabledxvk -forceuid3d9";
     BOOL result = CreateProcessW(
         NULL,
         &commandLine[0],

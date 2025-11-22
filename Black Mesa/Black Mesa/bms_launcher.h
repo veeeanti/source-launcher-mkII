22e74376b3d288eb3d53a8de1586949773af6878
vee anti <113473346+veeeanti@users.noreply.github.com>
2025-11-21 20:09:50 -0600

    make hl2 legacy branch

--- Black Mesa/Black Mesa/bms_launcher.h
+++
@@ -1,9 +0,0 @@
-#ifndef BMS_LAUNCHER_H
-#define BMS_LAUNCHER_H
-
-#include <string>
-
-// Function declarations
-bool fileExists(const std::string& filePath);
-
-#endif // BMS_LAUNCHER_H
--- Half-Life Source/hl1_launcher.h
+++
@@ -1,9 +0,0 @@
-#ifndef HL1_LAUNCHER_H
-#define HL1_LAUNCHER_H
-
-#include <string>
-
-// Function declarations
-bool fileExists(const std::string& filePath);
-
-#endif // HL1_LAUNCHER_H

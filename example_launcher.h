138e32423e790adff37029eb12a8c78fdff22a62
veeλnti <113473346+veeeanti@users.noreply.github.com>
2026-05-31 06:00:06 +0000

    upload example src files finally

---
+++ example_launcher.h
@@ -0,0 +1,9 @@
+#ifndef EXAMPLE_LAUNCHER_H
+#define EXAMPLE_LAUNCHER_H
+
+#include <string>
+
+// Function declarations
+bool fileExists(const std::string& filePath);
+
+#endif // EXAMPLE_LAUNCHER_H
